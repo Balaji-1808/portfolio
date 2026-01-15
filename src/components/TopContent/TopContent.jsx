a3d4427eb8c91805f18eac897724d4c48958a2c9
balaji_s <133853217+Balaji-1808@users.noreply.github.com>
2026-01-15 20:12:30 +0530

    Opens CV download link in a new tab
    Added target='_blank' to the CV download link.

--- src/components/TopContent/TopContent.jsx
+++ src/components/TopContent/TopContent.jsx
@@ -8,7 +8,7 @@ const TopContent = () => {
         <div className='topContent__container'>
             <h1>Mr.Balaji S</h1>
             <p>A professional Web and App Developer</p>
-            <a href='https://drive.google.com/file/d/1xqLr_yU4UZQjOnGwNP4xb-zeNKY9m770/view?usp=sharing'>
+            <a href='https://drive.google.com/file/d/1xqLr_yU4UZQjOnGwNP4xb-zeNKY9m770/view?usp=sharing' target='_blank'>
               <button className='topContent__downloadButton'>Download CV</button>
             </a>
             
